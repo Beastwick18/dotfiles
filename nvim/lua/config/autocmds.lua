edbfe6825cf7364fdd29ea7aeec0348599e2c7d7
Brad Culwell <bradculwell@pm.me>
2024-01-18 00:32:23 -0600

    01-18-24 00:32:23

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,3 +5,30 @@
 vim.cmd([[
   autocmd TermOpen * startinsert
 ]])
+
+function OpenDiagnosticIfNoFloat()
+  for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if vim.api.nvim_win_get_config(winid).zindex then
+      return
+    end
+  end
+  -- THIS IS FOR BUILTIN LSP
+  vim.diagnostic.open_float(nil, {
+    scope = "cursor",
+    focusable = false,
+    close_events = {
+      "CursorMoved",
+      "CursorMovedI",
+      "BufHidden",
+      "InsertCharPre",
+      "WinLeave",
+    },
+  })
+end
+
+vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+  pattern = "*",
+  callback = OpenDiagnosticIfNoFloat,
+  group = "lsp_diagnostics_hold",
+})
